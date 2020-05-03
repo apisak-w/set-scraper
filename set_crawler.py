872474ef9c77ec05daf49b0715374e5f8a7004ce
apisak-w <apisak.wongkhempetch@gmail.com>
2020-05-03 17:41:34 +0700

    Add extra value

--- set_crawler.py
+++ set_crawler.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 import re
+import xlwt
 
 
 class set_crawler:
@@ -19,10 +20,15 @@ class set_crawler:
         self.stock_info = dict()
         self.financial_period_cols_header = []
         self.financial_statistics_cols_header = []
+        self.factsheet_statistics_cols_header = []
 
     def go_to_company_highlights_page(self):
         self.driver.get(
             "https://www.set.or.th/set/companyhighlight.do?symbol=" + self.stock_code + "&ssoPageId=5&language=th&country=TH")
+
+    def go_to_factsheet_page(self):
+        self.driver.get(
+            "https://www.set.or.th/set/factsheet.do?symbol=" + self.stock_code + "&ssoPageId=3&language=th&country=TH")
 
     def get_company_info(self):
         print('Getting company info for stock code: ' + self.stock_code)
@@ -63,6 +69,17 @@ class set_crawler:
                 column_dict['text'] = column.text
                 self.financial_statistics_cols_header.append(column_dict)
 
+    def get_factsheet_statistics_column_header(self):
+        column_list = self.driver.find_elements_by_xpath(
+            "/html[@class='no-js']/body/table/tbody/tr[3]/td/table[@class='table-factsheet-padding3'][2]/tbody/tr[4]/td[2]/table[@class='table-factsheet-padding0'][1]/tbody/tr[2]/td[@class='factsheet-head']")
+        
+        for index, column in enumerate(column_list):
+            column_dict = dict()
+            if not column.text.startswith("ข้อมูลสถิติ"):
+                column_dict['index'] = index + 1
+                column_dict['text'] = column.text.replace("\n", " - ")
+                self.factsheet_statistics_cols_header.append(column_dict)
+
     def get_net_profit(self):
         profit_list = []
         for index, column in enumerate(self.financial_period_cols_header):
@@ -76,7 +93,7 @@ class set_crawler:
         for index, column in enumerate(self.financial_statistics_cols_header):
             pe = self.driver.find_element_by_xpath(
                 "/html[@class='no-js']/body/div[@class='container']/div[@class='row sidebar-body-content']/div[@id='body-content']/div[@class='row']/div[@id='maincontent']/div[@class='row']/div[@class='table-responsive']/table[@class='table table-hover table-info']/tbody[2]/tr[4]/td[" + str(column.get("index")) + "]").text
-            pe_list.append(pe)
+            pe_list.append(pe.strip())
         self.stock_info['pe_list'] = pe_list
 
     def get_bv_value(self):
@@ -84,8 +101,51 @@ class set_crawler:
         for index, column in enumerate(self.financial_statistics_cols_header):
             bv = self.driver.find_element_by_xpath(
                 "/ html[@class='no-js']/body/div[@class='container']/div[@class='row sidebar-body-content']/div[@id='body-content']/div[@class='row']/div[@id='maincontent']/div[@class='row']/div[@class='table-responsive']/table[@class='table table-hover table-info']/tbody[2]/tr[6]/td[" + str(column.get("index")) + "]").text
-            bv_list.append(bv)
+            bv_list.append(bv.strip())
         self.stock_info['bv_list'] = bv_list
+
+    def get_p_bv_value(self):
+        p_bv_list = []
+        for index, column in enumerate(self.financial_statistics_cols_header):
+            p_bv = self.driver.find_element_by_xpath(
+                "/html[@class='no-js']/body/div[@class='container']/div[@class='row sidebar-body-content']/div[@id='body-content']/div[@class='row']/div[@id='maincontent']/div[@class='row']/div[@class='table-responsive']/table[@class='table table-hover table-info']/tbody[2]/tr[5]/td[" + str(column.get("index")) + "]").text
+            p_bv_list.append(p_bv.strip())
+        self.stock_info['p_bv_list'] = p_bv_list
+
+    def get_roa_value(self):
+        roa_list = []
+        for index, column in enumerate(self.financial_period_cols_header):
+            roa = self.driver.find_element_by_xpath(
+                "/html[@class='no-js']/body/div[@class='container']/div[@class='row sidebar-body-content']/div[@id='body-content']/div[@class='row']/div[@id='maincontent']/div[@class='row']/div[@class='table-responsive']/table[@class='table table-hover table-info']/tbody[1]/tr[10]/td[" + str(column.get("index"))+ "]").text
+            roa_list.append(roa.strip())
+        self.stock_info['roa_list'] = roa_list
+
+    def get_roe_value(self):
+        roe_list = []
+        for index, column in enumerate(self.financial_period_cols_header):
+            roe = self.driver.find_element_by_xpath(
+                "/html[@class='no-js']/body/div[@class='container']/div[@class='row sidebar-body-content']/div[@id='body-content']/div[@class='row']/div[@id='maincontent']/div[@class='row']/div[@class='table-responsive']/table[@class='table table-hover table-info']/tbody[1]/tr[11]/td[" + str(column.get("index")) + "]").text
+            roe_list.append(roe.strip())
+        self.stock_info['roe_list'] = roe_list
+
+    def get_eps_value(self):
+        eps_list = []
+        for index, column in enumerate(self.financial_period_cols_header):
+            eps = self.driver.find_element_by_xpath(
+                "/html[@class='no-js']/body/div[@class='container']/div[@class='row sidebar-body-content']/div[@id='body-content']/div[@class='row']/div[@id='maincontent']/div[@class='row']/div[@class='table-responsive']/table[@class='table table-hover table-info']/tbody[1]/tr[8]/td[" + str(column.get("index")) + "]").text
+            eps_list.append(eps.strip())
+        self.stock_info['eps_list'] = eps_list
+
+    def get_beta_value(self):
+        beta_list = []
+        for index, column in enumerate(self.factsheet_statistics_cols_header):
+            beta = self.driver.find_element_by_xpath(
+                "/html[@class='no-js']/body/table/tbody/tr[3]/td/table[@class='table-factsheet-padding3'][2]/tbody/tr[4]/td[2]/table[@class='table-factsheet-padding0'][1]/tbody/tr[11]/td[@class='factsheet'][" + str(column.get("index")) + "]").text
+            beta_list.append(beta.strip())
+        self.stock_info['beta_list'] = beta_list
+
+    def get_benefit_value(self):
+        self.stock_info['benefit_value'] = "https://www.set.or.th/set/companyrights.do?symbol=" + self.stock_code + "&ssoPageId=7&language=th&country=TH"
 
     def get_company_highlights(self):
         self.go_to_company_highlights_page()
@@ -94,6 +154,14 @@ class set_crawler:
         self.get_net_profit()
         self.get_pe_value()
         self.get_bv_value()
+        self.get_p_bv_value()
+        self.get_roa_value()
+        self.get_roe_value()
+        self.get_eps_value()
+        self.go_to_factsheet_page()
+        self.get_factsheet_statistics_column_header()
+        self.get_beta_value()
+        self.get_benefit_value()
 
     def retrieve_stock_info(self):
         self.get_company_info()
@@ -107,6 +175,7 @@ class set_crawler:
         print("📈 self.stock_info", self.stock_info)
         # print("✅ self.financial_period_cols_header", self.financial_period_cols_header)
         # print("✅ self.financial_statistics_cols_header", self.financial_statistics_cols_header)
+        print("✅ self.factsheet_statistics_cols_header", self.factsheet_statistics_cols_header)
         print("\n ")
 
 
